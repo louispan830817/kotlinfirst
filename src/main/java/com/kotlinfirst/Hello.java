4754f80aa79ca00118b98688497897cd67814d74
Louis <louispan830817@gmail.com>
2020-03-19 01:36:58 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/kotlinfirst/Hello.java
+++ src/main/java/com/kotlinfirst/Hello.java
@@ -6,5 +6,16 @@ public class Hello {
         //new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 26;
+        Integer age2 = 19;
+        char a = '一';
+        Character a2 = '我';
+        byte b = 120;
+        float weight = 66.5f;
+
+        double height = 1.7;
+        boolean adult = true;
+        String name = "Louis";
+
     }
 }
--- src/main/kotlin/com/kotlinfirdt/Hello.kt
+++ src/main/kotlin/com/kotlinfirdt/Hello.kt
@@ -5,6 +5,11 @@ fun main() {
 //    Human().hello();
     val h = Human()
     h.hello()
+    var age : Int = 19;//var可變val不可變
+    var name = "我是誰";
+    var weight = 66.5f;
+
+
 }
 
 class Human {
